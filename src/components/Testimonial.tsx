e6b32e5e0fdf37191dc139175df406f93fe9b9ea
GisellyRock <gisellydsr@gmail.com>
2023-09-01 21:00:17 -0300

    first commit

---
+++ src/components/Testimonial.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Testimonial = () => {
+  return (
+    <div>
+      <div className='container pt-16 pb-16 grid-cols-2'>
+        <h2 className='font-medium text-2xl pb-4'> Testimonials</h2>
+        </div>
+    </div>
+    
+  )
+}
+
+export default Testimonial
